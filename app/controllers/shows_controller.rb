7ec7218339d7c6a29e2e37d37552c60db83d7252
Nick Riddell <nickjriddell@gmail.com>
2016-01-10 15:05:51 +0000

    initial show and venue views complete

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -2,4 +2,48 @@ class ShowsController < ApplicationController
   def index
     @shows = Show.all
   end
+
+  def new
+    @show = Show.new
+    @venue = Venue.all
+  end
+
+  def create
+    Show.create(show_params)
+    venue = Venue.find(params["show"] ["venue_id"].to_i)
+    venue.shows << show
+    redirect_to(shows_path)
+  end
+
+  def show
+    load_show
+  end
+
+  def edit
+    load_show
+    @venue = Venue.all
+  end
+
+  def update
+    load_show
+    venue = Venue.find(params["show"] ["venue_id"].to_i)
+    venue.shows << show
+    @show.update(show_params)
+    redirect_to(shows_path)
+  end  
+
+  def destroy
+    load_show
+    show.destroy
+    redirect_to( shows_path )
+  end
+
+  private
+  def show_params
+    params.require(:show).permit(:name, :image, :description, :venue_id, :performer_id)
+  end
+
+  def load_show
+    @show = Show.find(params[:id])
+  end
 end
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,4 +1,43 @@
 class VenuesController < ApplicationController
   def index
+    @venues = Venue.all
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    Venue.create(venue_params)
+    redirect_to( venues_path )
+  end
+
+  def show
+    load_venue
+  end
+
+  def edit
+    load_venue
+  end
+
+  def update
+    load_venue
+    venue.update(venue_params)
+    redirect_to(venues_path)
+  end
+
+  def destroy
+    load_venue
+    venue.destroy
+    redirect_to( venues_path )
+  end
+
+  private
+  def venue_params
+    params.require(:venue).permit(:name, :description, :image, :location)
+  end
+
+  def load_venue
+    @venue = Venue.find(params[:id])
   end
 end
