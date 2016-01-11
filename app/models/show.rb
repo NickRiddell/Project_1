9cc53471f4e4b1dc64734a52dc11dc471a58779e
Nick Riddell <nickjriddell@gmail.com>
2016-01-11 16:41:44 +0000

    shows displaying date and time in a human readable fashion

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,4 +9,30 @@ class Show < ActiveRecord::Base
   def self.search(query)
     where("name || description like ?", "%#{query}%")
   end
+
+  def human_readable_start_date
+     start_time.strftime('%A, %B %d %Y')
+  end
+
+   def human_readable_start_time
+     start_time.strftime('%H:%M')
+   end
+
+   def human_readable_end_date
+     end_time.strftime('%A, %B %d %Y')
+   end
+
+   def human_readable_end_time
+     end_time.strftime('%H:%M')
+   end
+
+   def self.events_at_venue(v_id)
+     where(venue_id:v_id)
+   end
+
+   def event_overlaps?(venue_id)
+     Event.events_at_venue(venue_id).all? do |event|
+       (event.event_date <= end_time) and (event_date >= event.end_time)  
+     end
+   end
 end
