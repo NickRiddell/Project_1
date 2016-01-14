6ebe0380b548945deebae8b143e05945c9bf9a15
Nick Riddell <nickjriddell@gmail.com>
2016-01-14 08:33:59 +0000

    footer added

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( normalize.css )
 Rails.application.config.assets.precompile += %w( header.png )
+Rails.application.config.assets.precompile += %w( normalize.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
